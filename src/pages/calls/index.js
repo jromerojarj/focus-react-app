7e489eacfbbfa4bcace0a97eef824aa27a595231
Jonathan Romero jromerojarj <jromerojarj@gmail.com>
2020-05-06 05:46:50 -0600

    Get calls to API

--- src/pages/calls/index.js
+++ src/pages/calls/index.js
@@ -13,6 +13,29 @@ import { TemplateUI } from "../../components/";
 import { Container, Typography } from "@material-ui/core";
 
 class Calls extends Component {
+  state = {
+    loading: true,
+    callsData: [],
+    error: false,
+  };
+
+  componentDidMount = async () => {
+    await this.getCalls();
+  };
+
+  getCalls = async () => {
+    const vmBoxID = "b37675a2d7b90d60f0ee5d4175502394";
+    const response = await this.props.getCallsList(vmBoxID);
+    if (response.status === "success") {
+      this.setState({
+        callsData: this.props.callsData.data,
+        error: false,
+        loading: false,
+      });
+    } else if (response.status === "error") {
+      this.setState({ callsData: [], error: true, loading: false });
+    }
+  };
   render() {
     return (
       <TemplateUI title="Calls" back={false}>
